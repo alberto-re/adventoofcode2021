38fc437dc06dd6e03941b788ddc326716f677cd1
Alberto Re <alberto.re@gmail.com>
2021-12-01 22:24:00 +0100

    Day 1

---
+++ day01.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# --- Day 1: Sonar Sweep ---
+
+from typing import List
+
+
+def count_increments(depths: List[int]) -> int:
+    return sum([depths[i] > depths[i - 1] for i in range(1, len(depths))])
+
+
+def sliding_windows(series: List[int], size: int = 3) -> List[int]:
+    return [sum(series[i : i + size]) for i in range(len(series) - size + 1)]
+
+
+with open("input/day01.txt") as f:
+    lines = [int(line.rstrip()) for line in f]
+
+print(f"Part one solution: {count_increments(lines)}")
+
+print(f"Part two solution: {count_increments(sliding_windows(lines))}")
